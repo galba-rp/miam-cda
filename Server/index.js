8a3fe3f4cb0ff007907214ee42c752c5abcbf926
unknown <yarolsav.hontar@gmail.com>
2020-12-22 22:21:08 +0100

    test

--- Server/index.js
+++ Server/index.js
@@ -11,19 +11,11 @@ const calcRoute = require('./routes/calculation');
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/calc', calcRoute);
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
-
-// app.use(express.static(path.join(__dirname, 'build')));
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
 
 
 //if environment variable PORT is not set then will use 5000
