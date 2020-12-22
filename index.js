c2353b1eae58b82916627e2a8ab7f4f546317916
unknown <yarolsav.hontar@gmail.com>
2020-12-22 15:48:22 +0100

    changed port

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get('/*', (req, res) => {
 
 
 //if environment variable PORT is not set then will use 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, ()=> console.log(`listening on port ${port}...`));
