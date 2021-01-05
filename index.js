f744e717cbdeed2fe68e5a332b9e301339062825
unknown <yarolsav.hontar@gmail.com>
2021-01-05 17:34:48 +0100

    added cors headers

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const calcRoute = require('./routes/calculation');
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/calc', calcRoute);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
+
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
